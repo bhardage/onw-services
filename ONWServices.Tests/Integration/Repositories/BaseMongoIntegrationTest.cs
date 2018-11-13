794d686d003967e821b53c9e34a02e3a3e650108
bhardage <bhardage@gmail.com>
2018-11-13 13:06:39 -0600

    Add test project
    Add integration tests for BaseRepository

---
+++ ONWServices.Tests/Integration/Repositories/BaseMongoIntegrationTest.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Options;
+using Mongo2Go;
+using MongoDB.Driver;
+using ONWServices.Repositories;
+using ONWServices.Settings;
+
+namespace ONWServices.Tests.Integration.Repositories
+{
+    public abstract class BaseMongoIntegrationTest
+    {
+        private const string DATABASE_NAME = "testdb";
+
+        protected static MongoDbContext CreateMongoDbContext()
+        {
+            MongoDbRunner runner = MongoDbRunner.Start();
+
+            return new MongoDbContext
+            {
+                Runner = runner,
+                Database = new MongoClient(runner.ConnectionString).GetDatabase(DATABASE_NAME),
+                DatabaseName = DATABASE_NAME
+            };
+        }
+
+        protected OnwDbContext CreateOnwDbContext(MongoDbContext dbContext)
+        {
+            IOptions<MongoDbSettings> settings = Options.Create<MongoDbSettings>(
+                new MongoDbSettings
+                {
+                    ConnectionString = dbContext.Runner.ConnectionString,
+                    Database = dbContext.DatabaseName
+                }
+            );
+
+            return new OnwDbContext(settings);
+        }
+    }
+}
---
+++ ONWServices.Tests/Integration/Repositories/MongoDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Mongo2Go;
+using MongoDB.Driver;
+
+namespace ONWServices.Tests.Integration.Repositories
+{
+    public class MongoDbContext
+    {
+        public MongoDbRunner Runner { get; set; }
+        public IMongoDatabase Database { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
