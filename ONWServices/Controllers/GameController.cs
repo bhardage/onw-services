d3ada297a0bb62e530f7361d94cfe57636ae139d
unknown <locadm@BPRUETT.xsinc.com>
2018-11-12 22:08:22 -0500

    AC ideas

--- ONWServices/Controllers/GameController.cs
+++ ONWServices/Controllers/GameController.cs
@@ -3,6 +3,13 @@ using ONWServices.ViewModels;
 
 namespace ONWServices.Controllers
 {
+
+    //Game statuses: New, Available, Night Phase, Information Review, Discussion, Round Summary, (Back to Night Phase), Game Summary, Closed
+    //Game must have a minimum of 3 players
+    //Game must have 1 Warewolf, Seer, Robber, and Troublemaker
+    //Game can only have 1 Game Master
+
+
     [Route("api/games")]
     [ApiController]
     public class GamesController : ControllerBase
