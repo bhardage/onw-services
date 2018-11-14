6ee30186f52017131229566e0347302e12c09a3e
bhardage <bhardage@gmail.com>
2018-11-14 14:59:49 -0600

    Serialize Game.GameId to mongodb as a string instead of bytes

--- ONWServices/Models/Game.cs
+++ ONWServices/Models/Game.cs
@@ -1,5 +1,6 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
+using ONWServices.Models.Serializers;
 using System;
 
 namespace ONWServices.Models
@@ -8,6 +9,7 @@ namespace ONWServices.Models
     public class Game : BaseDocument
     {
         [BsonElement("gameId")]
+        [BsonSerializer(typeof(GuidStringSerializer))]
         public Guid GameId { get; set; }
     }
 }
---
+++ ONWServices/Models/Serializers/GuidStringSerializer.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using System;
+
+namespace ONWServices.Models.Serializers
+{
+    /// <summary>
+    /// Serializes <c>Guid</c>'s to <c>string</c>'s and back.
+    /// </summary>
+    /// <remarks>
+    /// The default <c>Guid</c> serializer, <c>MongoDB.Bson.Serialization.Serializers.GuidSerializer</c>,
+    /// serializes <c>Guid</c>'s as <c>byte</c> arrays. This class opts to serialize using the
+    /// much-more-readable <c>string</c> format.
+    /// </remarks>
+    public class GuidStringSerializer : SerializerBase<Guid>
+    {
+        public override void Serialize(BsonSerializationContext context, BsonSerializationArgs args, Guid value)
+        {
+            context.Writer.WriteString(value.ToString());
+        }
+
+        public override Guid Deserialize(BsonDeserializationContext context, BsonDeserializationArgs args)
+        {
+            return new Guid(context.Reader.ReadString());
+        }
+    }
+}
