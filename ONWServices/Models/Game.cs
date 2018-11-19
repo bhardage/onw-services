0b3846472f06707faba288c0cac6ceb886ba8768
unknown <distantsound@gmail.com>
2018-11-19 17:18:17 -0500

    Add Unit Tests

--- ONWServices/Models/Game.cs
+++ ONWServices/Models/Game.cs
@@ -2,14 +2,42 @@
 using MongoDB.Bson.Serialization.Attributes;
 using ONWServices.Models.Serializers;
 using System;
+using System.Collections.Generic;
 
 namespace ONWServices.Models
 {
     [BsonIgnoreExtraElements]
     public class Game : BaseDocument
     {
+        public Game() {
+            Status = GameStatus.New;
+            SelectedRoles = new List<Role>
+            {
+                Role.Warewolf,
+                Role.Seer,
+                Role.Robber,
+                Role.Troublemaker
+            };
+        }
+
         [BsonElement("gameId")]
         [BsonSerializer(typeof(GuidStringSerializer))]
         public Guid GameId { get; set; }
+
+        public GameStatus Status { get; set; }
+        public List<Role> SelectedRoles { get; set; }
+    }
+
+    public enum GameStatus
+    {
+        New
+    }
+
+    public enum Role
+    {
+        Warewolf,
+        Troublemaker,
+        Seer,
+        Robber
     }
 }
