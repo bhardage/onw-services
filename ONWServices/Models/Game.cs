abfa31d4780e741654d33c39821adadc1ac4ab2e
Distantsound <Distantsound@gmail.com>
2018-12-15 12:24:16 -0500

    Revert "Add Unit Tests (#6)"
    This reverts commit ee6d7dd6e5a33814402ad9e4c5c1afa14d2d0812.

--- ONWServices/Models/Game.cs
+++ ONWServices/Models/Game.cs
@@ -2,27 +2,14 @@
 using MongoDB.Bson.Serialization.Attributes;
 using ONWServices.Models.Serializers;
 using System;
-using System.Collections.Generic;
 
 namespace ONWServices.Models
 {
     [BsonIgnoreExtraElements]
     public class Game : BaseDocument
     {
-        public Game() {
-            Status = GameStatus.New;            
-        }
-
         [BsonElement("gameId")]
         [BsonSerializer(typeof(GuidStringSerializer))]
         public Guid GameId { get; set; }
-
-        public GameStatus Status { get; set; }
-        public List<PlayerRole> SelectedRoles { get; set; }
-    }
-
-    public enum GameStatus
-    {
-        New
     }
 }
