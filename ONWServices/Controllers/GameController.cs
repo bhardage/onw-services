ee6d7dd6e5a33814402ad9e4c5c1afa14d2d0812
Distantsound <Distantsound@gmail.com>
2018-12-15 12:24:02 -0500

    Add Unit Tests (#6)
    * Add Unit Tests
    
    * PR changes
    
    * Rename NewGameTest

--- ONWServices/Controllers/GameController.cs
+++ ONWServices/Controllers/GameController.cs
@@ -4,14 +4,19 @@ using ONWServices.ViewModels;
 namespace ONWServices.Controllers
 {
 
-    //Game statuses: New, Available, Night Phase, Information Review, Discussion, Round Summary, (Back to Night Phase), Game Summary, Closed
-    //Game must have a minimum of 3 players
-    //Game must have 1 Warewolf, Seer, Robber, and Troublemaker
-    //Game can only have 1 Game Master
-
-        //test
-
-
+    //Game Flow
+    //
+    //User Creates game, becomes Game Master, tells Players Game ID
+    //Master sets up game settings:  Timer length, Available Roles,
+    //Available roles based on number of players (# number of players + 3)
+    //Players join game
+    //when all players have joined, Master initiates Night Phase
+    //Players see their role
+    //Players submit their actions by choosing players based on their role
+    //When all turns are submitted, Master initiates Information Phase
+    //This turns on a game timer
+    //players sees their new information, discuss what they saw, choose who to shoot
+    //At the end of time timer, the game tallies who is shot and announces winner  
 
     [Route("api/games")]
     [ApiController]
@@ -28,6 +33,12 @@ namespace ONWServices.Controllers
         public ActionResult<GameViewModel> GetGame(string id)
         {
             return new GameViewModel() { id = id };
+        }
+
+        [HttpPost]
+        public ActionResult<GameViewModel> UpdateGameSettings(string id, int gameState)    
+        {
+            return new GameViewModel() { };
         }
 
         [HttpDelete("{id}")]
